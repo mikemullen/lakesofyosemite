18c44bc4036a2b310d09c36de82855605a0b10b0
Mike Mullen <m@m-mullen.com>
2020-10-09 17:28:26 -0700

    add static files2

--- lakesofyosemite/settings.py
+++ lakesofyosemite/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static')
-# ]
+   os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'lakesofyosemite/media')
