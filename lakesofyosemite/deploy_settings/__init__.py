c0e9845a15e49c0b505f585ec7ff3c9e27deaf2a
Mike Mullen <m@m-mullen.com>
2020-10-09 17:18:00 -0700

    add whitenoise to settings again7

--- lakesofyosemite/deploy_settings/__init__.py
+++ lakesofyosemite/deploy_settings/__init__.py
@@ -16,4 +16,4 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
