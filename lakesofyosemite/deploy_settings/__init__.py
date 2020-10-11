7dc845381da9966ea2a152eb88408980f2507ace
Mike Mullen <m@m-mullen.com>
2020-10-11 15:09:10 -0700

    add dropbox2

--- lakesofyosemite/deploy_settings/__init__.py
+++ lakesofyosemite/deploy_settings/__init__.py
@@ -19,6 +19,7 @@ DATABASES['default'].update(db_from_env)
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 DROPBOX_OAUTH2_TOKEN = 'sl.AjYw4hWhIrlFk4Mb0j1QvhURZbFK1vVSpjS8OeBFTMQmS4mr-JbbN5nt7kZAo6hGIs8laVza8Wd9Iohx-AA8JasVuDcRUbbnLx69j5h9kSRncwFv2i1thz9uwmwphh8EVDWDyMI'
 
