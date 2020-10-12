ad2a968f0a3803a7eb28a282310b7f278a0bc1f5
Mike Mullen <m@m-mullen.com>
2020-10-11 21:05:27 -0700

    add storages try3

--- lakesofyosemite/settings.py
+++ lakesofyosemite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lakes.apps.LakesConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'storages',
 ]
 
 ROOT_URLCONF = 'lakesofyosemite.urls'
