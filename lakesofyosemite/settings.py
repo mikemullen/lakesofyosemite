e30b25a42232eea9c2283572ab3af783d9b224a3
Mike Mullen <m@m-mullen.com>
2021-02-09 18:47:43 -0800

    typeo

--- lakesofyosemite/settings.py
+++ lakesofyosemite/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fd^*tx+@+mlp4p-qe(cw==+v=6yk=wb19uekvzpz#7#u36cz#%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Fales
+DEBUG = False
 
 ALLOWED_HOSTS = ['*','lakesofyosemite.com']
 
